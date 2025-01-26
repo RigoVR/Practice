4b1bf8af87d139ed9d2006add997c467f421eaca
Rigo Razo <rigovr7@gmail.com>
2025-01-26 12:24:41 -0500

    Updated for loop and added user input

--- forloop.java
+++ forloop.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
+
 public class forloop {
     public static void main(String[] args) {
-        for ( int i = 0; i <10; i++){
-            System.out.println("This is a for loop");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("How many times would you like to loop your message? : ");
+        int numOfLoops = input.nextInt();
+            for ( int i = 1; i <= numOfLoops; i++){
+            System.out.println(i + ": This is a for loop");
         }
     }
 }
