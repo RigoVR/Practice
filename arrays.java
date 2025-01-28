44abc95bafa0f25b60db475a044a331789f151c1
Rigo Razo <rigovr7@gmail.com>
2025-01-28 11:43:57 -0500

    Updated arrays for user input

--- arrays.java
+++ arrays.java
@@ -1,8 +1,29 @@
+
+import java.util.Scanner;
 public class arrays {
     public static void main(String[] args) {
-        for ( int i = 0 ; i< 20 ; i++){
-            System.out.println("Hello ");
+        Scanner input = new Scanner(System.in);
+        System.out.print("How many numbers do you want to input: ");
+        int size = input.nextInt();
+
+        double[] myArray = new double[size];
+
+        for(int i = 0; i < myArray.length; i++){
+            System.out.print("Enter Value " + (i+1) + ": ");
+            myArray[i] = input.nextDouble();
         }
+
+        double total = 0;
+
+        for(int i = 0; i < myArray.length; i++ ){
+            total = total + myArray[i];
+        }
+
+
+        System.out.println("The Total is : " + total);
+
+
+
 
 
     }
